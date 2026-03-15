10bb8fefedaf5832d143bea0e7feedaf504157fa
Leah Beder <leahbeder@Leahs-iMac.local>
2026-03-15 14:40:46 -0500

    serialization

--- server/models.py
+++ server/models.py
@@ -1,32 +1,100 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
-from marshmallow import Schema, fields
+from sqlalchemy_serializer import SerializerMixin
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
+    serialize_rules = (
+        '-reviews.customer',
+        '-items.reviews',
+        '-items.customers',
+    )
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+    customers = association_proxy('reviews', 'customer')
+
+    serialize_rules = (
+        '-reviews.item',
+        '-customers.reviews',
+        '-customers.items',
+    )
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    serialize_rules = (
+        '-customer.reviews',
+        '-customer.items',
+        '-item.reviews',
+        '-item.customers',
+    )
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+
+class CustomerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        load_instance = True
+        include_fk = True
+
+    reviews = fields.Nested('ReviewSchema', many=True, exclude=('customer',))
+    items = fields.List(fields.Nested('ItemSchema', exclude=('reviews', 'customers')))
+
+
+class ItemSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Item
+        load_instance = True
+        include_fk = True
+
+    reviews = fields.Nested('ReviewSchema', many=True, exclude=('item',))
+    customers = fields.List(fields.Nested('CustomerSchema', exclude=('reviews', 'items')))
+
+
+class ReviewSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Review
+        load_instance = True
+        include_fk = True
+
+    customer = fields.Nested('CustomerSchema', exclude=('reviews', 'items'))
+    item = fields.Nested('ItemSchema', exclude=('reviews', 'customers'))
